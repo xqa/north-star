de4dde7596139d53a7970ab524423a9e5feeb68a
minge <i@minge.dev>
2022-12-11 18:38:28 +0800

    u

--- Task/cron_daily.js
+++ Task/cron_daily.js
@@ -1,6 +1,5 @@
 /*
-README：https://github.com/yichahucha/surge/tree/master
 每日蚂蚁收能量提醒（corn "11 7 * * *" 每天7:11）+ 每日壹句（有道词典）+ 点击通知跳转支付宝蚂蚁森林页面
 */
-$notify("每日蚂蚁收能量提醒", "", "收能量啦👉alipay://platformapi/startapp?appId=60000002", {"open-url" : "alipay://platformapi/startapp?appId=60000002"});
+$notify("每日提醒", "", "收能量啦👉", {"open-url" : "alipay://platformapi/startapp?appId=60000002"});
 $done();
