0739457460a6df4528944aa3b8be2bbe3b9b45c5
minge <i@minge.dev>
2023-01-09 04:29:11 +0800

    u

--- Crack/qj.js
+++ Crack/qj.js
@@ -5,15 +5,27 @@
 *******************************
 [rewrite_local]
 ^https:\/\/qianji\.xxoojoke\.com\/vip\/configios url script-response-body https://raw.githubusercontent.com/xqa/north-star/main/Crack/qj.js
+^https:\/\/qianji\.xxoojoke\.com\/client\/init url script-response-body https://raw.githubusercontent.com/xqa/north-star/main/Crack/qj.js
 [mitm] 
 hostname = qianji.xxoojoke.com
 *******************************/
-
-let body = JSON.parse($response.body);
-body.data.config.userinfo = {
-  ...body.data.config.userinfo,
+const body = JSON.parse($response.body);
+const hack = {
   vipend: 2365847580,
   viptype: 100,
   vipstart: 1365847580,
-};
+}
+if($request.url.match("vip/configios")){
+  body.data.config.userinfo = {
+    ...body.data.config.userinfo,
+    ...hack
+  };
+}
+if($request.url.match("client/init")){
+  body.data.userinfo = {
+    ...body.data.userinfo,
+    ...hack
+  };
+}
+
 $done({ body: JSON.stringify(body) });
